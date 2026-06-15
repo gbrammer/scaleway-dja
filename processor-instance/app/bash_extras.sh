b0a7319c864fb828cd15f7d803159f8c826a2271
Gabriel Brammer <gbrammer@gmail.com>
2026-06-15 11:48:12 +0200

    add dubig

--- processor-instance/app/bash_extras.sh
+++ processor-instance/app/bash_extras.sh
@@ -54,3 +54,24 @@ count_flt() {
     done
 
 }
+
+## list large files
+dubig() {
+
+    files=`ls`
+
+    du -sc $files | sort -nr | awk '
+         BEGIN {
+            split("KB,MB,GB,TB", Units, ",");
+         }
+         {
+            u = 1;
+            while ($1 >= 1024) {
+               $1 = $1 / 1024;
+               u += 1
+            }
+            $1 = sprintf("%5.1f %s", $1/2, Units[u]);
+            print $0;
+         }' | grep -v KB
+
+}
